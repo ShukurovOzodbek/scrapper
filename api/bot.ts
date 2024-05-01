fb628b750adf7ea42ee65381173e103f6a090da1
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-01 22:12:56 +0500

    vercel deploy

--- api/bot.ts
+++ api/bot.ts
@@ -1,9 +1,8 @@
-import {Bot} from "grammy";
-import {utils} from "./utils";
+import {Bot, webhookCallback} from "grammy";
 
-const bot = new Bot("6648912671:AAE3CGAZKkuoBnYSgKnA3XPd8zknYQY3zSk");
+const token = "6648912671:AAE3CGAZKkuoBnYSgKnA3XPd8zknYQY3zSk";
+if (!token) throw new Error("BOT_TOKEN is unset");
 
+const bot = new Bot(token);
 
-bot.use(utils);
-
-bot.start();
+export default webhookCallback(bot, "std/http");
