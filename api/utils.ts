ab7eda9e93789db8ec35e8d0487ce7b02fa77d4a
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-05-08 16:45:30 +0500

    removed all for testing

--- api/utils.ts
+++
@@ -1,9 +0,0 @@
-import {Composer} from "grammy";
-
-export const utils = new Composer();
-
-utils.on('message', async (ctx) => {
-    const message: any = ctx.message.text;
-    
-    await ctx.reply(message)
-});
